a1affc1b084b2b980212bde64cfd6e5cc2959854
Patrick Towles <james.towles@gmail.com>
2022-06-27 03:14:00 -0400

    moved to 0.7

--- examples/main.rs
+++ examples/main.rs
@@ -39,7 +39,7 @@ fn setup(
 
     // camera
     commands
-        .spawn_bundle(Camera3dBundle {
+        .spawn_bundle(PerspectiveCameraBundle {
             transform: Transform::from_xyz(5.0, 2.0, -10.0).looking_at(Vec3::ZERO, Vec3::Y),
             ..default()
         })
