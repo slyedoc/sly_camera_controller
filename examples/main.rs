c47cb1437fc681b9471b83a8f4e14e6b1e9f82bb
Patrick Towles <james.towles@gmail.com>
2022-06-27 02:38:36 -0400

    should have broke this out long ago

---
+++ examples/main.rs
@@ -0,0 +1,49 @@
+use bevy::{prelude::*, window::PresentMode};
+use crate::*;
+
+fn main() {
+    App::new()
+        .insert_resource(WindowDescriptor {
+            present_mode: PresentMode::Fifo,
+            ..default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(CameraControllerPlugin)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // light
+    commands.spawn_bundle(DirectionalLightBundle {
+        transform: Transform::from_xyz(50.0, 50.0, 50.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+
+    // ground
+    commands
+        .spawn_bundle(PbrBundle {
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            mesh: meshes.add(Mesh::from(shape::Plane { size: 100.0 })),
+            material: materials.add(StandardMaterial {
+                base_color: Color::DARK_GREEN,
+                ..default()
+            }),
+            ..default()
+        })
+        .insert(Name::new("Ground"));
+
+    // camera
+    commands
+        .spawn_bundle(Camera3dBundle {
+            transform: Transform::from_xyz(5.0, 2.0, -10.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        })
+        // Add our controller
+        .insert(CameraController::default());
+    
+}
